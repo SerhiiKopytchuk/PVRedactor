f6794621a3b6db4382400aa04cd1e1104a4a24bc
Serhii <kopytchukserhii@gmail.com>
2022-12-27 18:45:07 +0200

    created project

---
+++ PVRedactor/PVRedactor/PVRedactorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PVRedactorApp.swift
+//  PVRedactor
+//
+//  Created by Serhii Kopytchuk on 27.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct PVRedactorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
